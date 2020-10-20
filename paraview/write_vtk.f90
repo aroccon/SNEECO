56213e39e33062ed552b34a9277d7c0cf6f88712
Alessio <keroro.90@hotmail.it>
2020-10-20 11:59:58 +0200

    paraview_ok

--- paraview/write_vtk.f90
+++ paraview/write_vtk.f90
@@ -5,54 +5,64 @@ use droplets
 integer :: t
 character(len=8) :: step
 character(len=40) :: file_name
+character(len=80) :: buffer
+character(len=10) :: lf
+character(len=16) :: str4
 
+
+lf=achar(10)
 print*,'Creating .vtk file for step',t
-
-!Creating file name
+write(str4(1:16),'(i16)') n_p
 write(step,'(I8.8)') t
 file_name='outparaview_'//step//'.vtk'
 
 !open file
-open(unit=99,file='output/'//file_name,form='formatted',status='new',action='write')
+open(unit=99,file='output/'//file_name,form='unformatted',status='new',access='stream',convert='big_endian')
 
-write(99,*) '# vtk DataFile Version 3.0'
-write(99,*) 'Unstructured grid legacy vtk file with point scalar data'
-write(99,*) 'ASCII'
-write(99,*) 'DATASET UNSTRUCTURED_GRID'
-write(99,*) 'POINTS', n_p, 'double'
-!write(66) trim(buffer)
+! write head of file
+buffer='# vtk DataFile Version 3.0'//lf
+write(99) trim(buffer)
+buffer='Particle data'//lf
+write(99) trim(buffer)
+buffer='BINARY'//lf
+write(99) trim(buffer)
+buffer='DATASET UNSTRUCTURED_GRID'//lf
+write(99) trim(buffer)
+buffer='POINTS '//str4//' float'//lf
+write(99) trim(buffer)
 do i=1,n_p
- write(99,*) x(i),y(i),0d0
+ write(99) real(x(i)),real(y(i)),real(0d0)
 enddo
 
-!buffer='CELL_TYPES'//str4//lf
-!write(66) trim(buffer)
-!do i=1,part_number
-! write(66) 1 ! cell type 1, VTK_VERTEX
-!enddo
 
-!buffer='POINT_DATA '//str4//lf
-!write(66) trim(buffer)
-!buffer='SCALARS position integer 1'//lf
-!write(66) trim(buffer)
-!buffer='LOOKUP_TABLE default'//lf
-!write(66) trim(buffer)
-!do i=1,part_number
-! write(66) 1 ! empty buffer
-!enddo
+buffer='CELL_TYPES'//str4//lf
+write(99) trim(buffer)
+do i=1,n_p
+ write(99) 1 ! cell type 1, VTK_VERTEX
+enddo
+
+buffer='POINT_DATA '//str4//lf
+write(99) trim(buffer)
+buffer='SCALARS position integer 1'//lf
+write(99) trim(buffer)
+buffer='LOOKUP_TABLE default'//lf
+write(99) trim(buffer)
+do i=1,n_p
+ write(99) 1 ! empty buffer
+enddo
 
 !if(partvelflag.eq.1)then
 ! buffer='VECTORS velocity float'//lf
 ! write(66) trim(buffer)
-! do i=1,part_number!
+! do i=1,part_number
 !  write(66) real(upar(i,1)),real(upar(i,2)),real(upar(i,3))
 ! enddo
 !endif
 
+buffer=lf
+write(99) trim(buffer)
+close(99,status='keep')
 
-
-
-close(99)
 
 return
 end
